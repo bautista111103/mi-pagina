1ecef1e37c58d8735c969a6f43d1693ee3319597
my_github_bautista leguizamo <leguizamobautista@gmail.com>
2022-11-23 16:36:22 -1200

    calltoaction

--- src/components/CallToAction.jsx
+++ src/components/CallToAction.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function CallToAction(){
-    return(
-    <div>
-        sopngiesogioes
-    </div>
+export default function CallToAction() {
+    return (
+        <div>
+            <button className="callToAction">
+                <span>Proximos Partidos</span><i></i>
+            </button>
+        </div>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,32 @@
 import React from "react";
-import ComponenteHome from '../components/Home'
-import '../styles/style.css'
+import '../styles/style.css';
+import CallToAction from "../components/CallToAction";
+// import ReactPlayer from 'react-player'
 
 
 
-export default function Home(){
-  return(
+
+export default function Home() {
+  return (
     <div className="contenedor-home">
-      <ComponenteHome/>
+      <div className="container-componente-home">
+        <div className="container-callToAction">
+          <CallToAction />
+        </div>
+        <div className="container-carousel">
+          <p>carousel</p>
+        </div>
+      </div>
     </div>
   )
 }
+/* <ReactPlayer
+url={require('../images/gol-maradona.mp4')}
+className='reproductor-video'
+playing
+width='100%'
+height='50vh'
+loop='true'
+volume={0}
+/> */
 
